9823f79059379893e74bdc27e27d96a0ece709ec
Mike Nacey <mike-nacey@gmail.com>
2021-08-17 13:00:07 -0400

    lots of changes: added other content types, need .env file inside of client folder now with REACT_APP_LOOKER_AUTH_URL and REACT_APP_LOOKER_HOST

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,8 +1,16 @@
 
 import {  Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
-const Dashboards = [1, 2, 3, 4, 5].map((db, idx) => (
+const Dashboards = [1, 2, 3, 4, 9].map((db, idx) => (
     <NavDropdown.Item key={idx} href={`/dashboard/${db}`}>Dashboard {db}</NavDropdown.Item>
+  ));
+
+const Looks = [3, 2, 7, 6, 8].map((db, idx) => (
+    <NavDropdown.Item key={idx} href={`/look/${db}`}>Look {db}</NavDropdown.Item>
+  ));
+
+  const Explores = [1, 2, 3, 4, 5].map((db, idx) => (
+    <NavDropdown.Item key={idx} href={`/explore/${db}`}>Explore {db}</NavDropdown.Item>
   ));
 
 export const NavigationBar = () => (
@@ -10,8 +18,14 @@ export const NavigationBar = () => (
     <Nav className="justify-content-start">
         <Navbar.Brand href="/">Analytics</Navbar.Brand>
         <Nav.Link href="/">Home</Nav.Link>
-        <NavDropdown title="Dashboards" id="basic-nav-dropdown">
+        <NavDropdown title="Dashboards" id="basic-nav-dropdown-dashboards">
         {Dashboards}
+        </NavDropdown>
+        <NavDropdown title="Looks" id="basic-nav-dropdown-looks">
+        {Looks}
+        </NavDropdown>
+        <NavDropdown title="Explores" id="basic-nav-dropdown-explores">
+        {Explores}
         </NavDropdown>
     </Nav>
     <Nav className="ml-auto">
